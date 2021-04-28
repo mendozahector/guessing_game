e850eed31ba30f25befad34105a9eef7364da201
Hector <hectormendoza@byui.edu>
2021-04-28 17:08:12 -0600

    introduction prompt

--- lab01.py
+++ lab01.py
@@ -11,9 +11,31 @@
 
 import random
 
+def main():
+    welcome_message()
+    difficulty = get_difficulty()
+
+    print(f"Guess a number between 1 and {difficulty}")
+
 # Game introduction
+def welcome_message():
+    print("This is the \"guess a number\" game.\n"
+        "You try to guess a random number in the smallest number of attempts.\n")
 
 # Prompt the user for how difficult the game will be. Ask for an integer
+def get_difficulty():
+    # Let's make sure we actually have an integer
+    while True:
+        try:
+            integer = int(input("Pick a positive integer (greater than 1): "))
+            if integer > 1:
+                break
+            else:
+                print("Please pick a number greater than 1.")
+        except ValueError:
+            print("You have entered an invalid integer.")
+    
+    return integer
 
 # Generate a random number between 1 and the chosen value
 #value_random = random.randint(1, value_max)
@@ -31,3 +53,6 @@ import random
     # Make a decision: was the guess too high, too low, or just right
 
 # Give the user a report: How many guesses and what the guesses where
+
+if __name__ == "__main__":
+    main()
