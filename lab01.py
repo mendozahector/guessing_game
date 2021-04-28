1f059bdd39bc72986bb12c28f299bbeb2f5d3fc2
Hector <hectormendoza@byui.edu>
2021-04-28 16:45:02 -0600

    first commit

---
+++ lab01.py
@@ -0,0 +1,33 @@
+# 1. Name:
+#    Hector Mendoza
+# 2. Assignment Name:
+#    Lab 01: Python Review
+# 3. Assignment Description:
+#    -describe what this program is meant to do-
+# 4. What was the hardest part? Be as specific as possible.
+#    -a paragraph or two about how the assignment went for you-
+# 5. How long did it take for you to complete the assignment?
+#    -total time in hours including reading the assignment and submitting the program-  
+
+import random
+
+# Game introduction
+
+# Prompt the user for how difficult the game will be. Ask for an integer
+
+# Generate a random number between 1 and the chosen value
+#value_random = random.randint(1, value_max)
+
+# Give the user instructions as to what he or she will be doing
+
+# Initialize the sentinal and the array of guesses
+
+# Play the guessing game
+
+    # Prompt the user for a number
+
+    # Store the number in an array so it can be displayed later
+
+    # Make a decision: was the guess too high, too low, or just right
+
+# Give the user a report: How many guesses and what the guesses where
